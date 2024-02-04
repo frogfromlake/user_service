7c1d706dd08e3ff18c6dc273d4f8b19c3e0ca5ca
Fabian Quist <frog_from_the_lake@gmx.de>
2024-02-04 15:53:02 +0100

    feat(grpc gapi):impl grpc server

---
+++ gapi/server.go
@@ -0,0 +1,34 @@
+package gapi
+
+import (
+	"fmt"
+
+	db "github.com/Streamfair/streamfair_user_svc/db/sqlc"
+	"github.com/Streamfair/streamfair_user_svc/pb"
+	"github.com/Streamfair/streamfair_user_svc/token"
+	"github.com/Streamfair/streamfair_user_svc/util"
+)
+
+// Server serves gRPC requests for the streamfair user management service.
+type Server struct {
+	pb.UnimplementedUserSvcServer
+	config          util.Config
+	store           db.Store
+	localTokenMaker token.Maker
+}
+
+// NewServer creates a new gRPC server.
+func NewServer(config util.Config, store db.Store) (*Server, error) {
+	localTokenMaker, err := token.NewLocalPasetoMaker(config.TokenSymmetricKey)
+	if err != nil {
+		panic(fmt.Sprintf("Failed to create local token maker: %v", err))
+	}
+
+	server := &Server{
+		config:          config,
+		store:           store,
+		localTokenMaker: localTokenMaker,
+	}
+
+	return server, nil
+}

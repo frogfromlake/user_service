19f3532bc0105279e4e9ec7b8a4e2f59ff1837e6
Fabian Quist <frog_from_the_lake@gmx.de>
2024-02-09 00:21:50 +0100

    debug ci

--- main.go
+++ main.go
@@ -13,27 +13,41 @@ import (
 
 func main() {
 	fmt.Println("Hello, Streamfair User Management Service!")
+
 	config, err := util.LoadConfig(".")
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "config: error while loading config: %v\n", err)
+		return
 	}
 
 	poolConfig, err := pgxpool.ParseConfig(config.DBSource)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "config: error while parsing config: %v\n", err)
+		return
 	}
 
 	conn, err := pgxpool.New(context.Background(), poolConfig.ConnString())
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "db connection: unable to create connection pool: %v\n", err)
+		return
 	}
 
 	store := db.NewStore(conn)
 	server, err := gapi.NewServer(config, store)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "server: error while creating server: %v\n", err)
+		return
 	}
 
-	go server.RunGrpcGatewayServer()
-	server.RunGrpcServer()
+	// Start gRPC gateway server in a separate goroutine
+	go func() {
+		if err := server.RunGrpcGatewayServer(); err != nil {
+			fmt.Fprintf(os.Stderr, "grpc gateway server: error while running: %v\n", err)
+		}
+	}()
+
+	// Start gRPC server
+	if err := server.RunGrpcServer(); err != nil {
+		fmt.Fprintf(os.Stderr, "grpc server: error while running: %v\n", err)
+	}
 }

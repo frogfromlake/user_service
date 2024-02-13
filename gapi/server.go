850b9cdf80bfe5370f481360646fc5177df4ca4c
Fabian Quist <frog_from_the_lake@gmx.de>
2024-02-13 14:45:07 +0100

    added http2 to grpc gateway server

--- gapi/server.go
+++ gapi/server.go
@@ -12,6 +12,8 @@ import (
 	"github.com/Streamfair/streamfair_user_svc/token"
 	"github.com/Streamfair/streamfair_user_svc/util"
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
+	"golang.org/x/net/http2"
+	"golang.org/x/net/http2/h2c"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/health"
 	"google.golang.org/grpc/health/grpc_health_v1"
@@ -22,6 +24,7 @@ import (
 // Server serves gRPC requests for the streamfair user management service.
 type Server struct {
 	grpcServer *grpc.Server
+	httpServer *http.Server
 	pb.UnimplementedUserServiceServer
 	config          util.Config
 	store           db.Store
@@ -31,17 +34,12 @@ type Server struct {
 
 // NewServer creates a new gRPC server.
 func NewServer(config util.Config, store db.Store) (*Server, error) {
-	localTokenMaker, err := token.NewLocalPasetoMaker(config.TokenSymmetricKey)
-	if err != nil {
-		panic(fmt.Sprintf("Failed to create local token maker: %v", err))
-	}
-
 	server := &Server{
 		config:     config,
 		store:      store,
 		grpcServer: grpc.NewServer(),
+		httpServer: &http.Server{Handler: nil},
 		healthSrv:  health.NewServer(),
-		localTokenMaker: localTokenMaker,
 	}
 
 	grpc_health_v1.RegisterHealthServer(server.grpcServer, server.healthSrv)
@@ -61,7 +59,7 @@ func (server *Server) RunGrpcServer() error {
 	if err != nil {
 		// Update the health status to NOT_SERVING if there's an error.
 		server.healthSrv.SetServingStatus("", grpc_health_v1.HealthCheckResponse_NOT_SERVING)
-		return fmt.Errorf("server: error while creating listener: %v", err)
+		return fmt.Errorf("server: error while creating gRPC listener: %v", err)
 	}
 
 	log.Printf("start gRPC server on %s", listener.Addr().String())
@@ -74,7 +72,7 @@ func (server *Server) RunGrpcServer() error {
 	return nil
 }
 
-// RunGrpcGatewayServer runs a gRPC gateway server on the given address.
+// RunGrpcGatewayServer runs a gRPC gateway server that translates HTTP requests into gRPC calls.
 func (server *Server) RunGrpcGatewayServer() error {
 	jsonOption := runtime.WithMarshalerOption(runtime.MIMEWildcard, &runtime.JSONPb{
 		MarshalOptions: protojson.MarshalOptions{
@@ -97,11 +95,17 @@ func (server *Server) RunGrpcGatewayServer() error {
 	mux := http.NewServeMux()
 	mux.Handle("/", grpcMux)
 
+	// Wrap the mux with h2c.NewHandler to support both HTTP/1 and HTTP/2 connections.
+	handler := h2c.NewHandler(mux, &http2.Server{})
+
+	// Set the httpServer handler to the wrapped mux.
+	server.httpServer.Handler = handler
+
 	// Add a route for the health check service.
 	mux.HandleFunc("/v1/healthz", func(w http.ResponseWriter, r *http.Request) {
 		resp, err := server.healthSrv.Check(context.Background(), &grpc_health_v1.HealthCheckRequest{})
 		if err != nil {
-			// Log the error and respond with an internal server error status code.
+			// Log the error for debugging purposes
 			log.Printf("Health check failed: %v", err)
 			w.WriteHeader(http.StatusInternalServerError)
 			w.Write([]byte("Internal Server Error"))
@@ -120,11 +124,11 @@ func (server *Server) RunGrpcGatewayServer() error {
 
 	listener, err := net.Listen("tcp", server.config.HttpServerAddress)
 	if err != nil {
-		return fmt.Errorf("server: error while creating listener: %v", err)
+		return fmt.Errorf("server: error while creating HTTP listener: %v", err)
 	}
 
 	log.Printf("start HTTP Gateway server on %s", listener.Addr().String())
-	err = http.Serve(listener, mux)
+	err = server.httpServer.Serve(listener)
 	if err != nil {
 		return fmt.Errorf("server: error while starting HTTP Gateway server: %v", err)
 	}
--- go.mod
+++ go.mod
@@ -13,6 +13,7 @@ require (
 	github.com/stretchr/testify v1.8.4
 	go.uber.org/mock v0.4.0
 	golang.org/x/crypto v0.18.0
+	golang.org/x/net v0.20.0
 	google.golang.org/genproto/googleapis/api v0.0.0-20240125205218-1f4bbc51befe
 	google.golang.org/grpc v1.61.0
 	google.golang.org/protobuf v1.32.0
@@ -60,7 +61,6 @@ require (
 	go.uber.org/multierr v1.9.0 // indirect
 	golang.org/x/arch v0.3.0 // indirect
 	golang.org/x/exp v0.0.0-20230905200255-921286631fa9 // indirect
-	golang.org/x/net v0.20.0 // indirect
 	golang.org/x/sync v0.5.0 // indirect
 	golang.org/x/sys v0.16.0 // indirect
 	golang.org/x/text v0.14.0 // indirect

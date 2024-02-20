c648a2edeb2d988316c00f50a607e41e67efd263
Fabian Quist <frog_from_the_lake@gmx.de>
2024-02-20 19:11:17 +0100

    removed return for now

--- main.go
+++ main.go
@@ -17,26 +17,22 @@ func main() {
 	config, err := util.LoadConfig(".")
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "config: error while loading config: %v\n", err)
-		return
 	}
 
 	poolConfig, err := pgxpool.ParseConfig(config.DBSource)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "config: error while parsing config: %v\n", err)
-		return
 	}
 
 	conn, err := pgxpool.New(context.Background(), poolConfig.ConnString())
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "db connection: unable to create connection pool: %v\n", err)
-		return
 	}
 
 	store := db.NewStore(conn)
 	server, err := gapi.NewServer(config, store)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "server: error while creating server: %v\n", err)
-		return
 	}
 
 	go server.RunGrpcGatewayServer()

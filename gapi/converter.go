abb5025a95c7af988687fd3a03815869ae3299dd
Fabian Quist <frog_from_the_lake@gmx.de>
2024-04-03 11:25:55 +0200

    add submodule

--- gapi/converter.go
+++ gapi/converter.go
@@ -1,8 +1,8 @@
 package gapi
 
 import (
+	pb "github.com/Streamfair/streamfair_user_svc/common_proto/UserService/pb/user"
 	db "github.com/Streamfair/streamfair_user_svc/db/sqlc"
-	pb "github.com/Streamfair/streamfair_user_svc/_common_proto/UserService/pb/user"
 	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
@@ -29,15 +29,15 @@ func convertUsersList(users []db.ListUsersRow) []*pb.Users {
 	var userList []*pb.Users
 	for _, user := range users {
 		userList = append(userList, &pb.Users{
-			Id:                user.ID,
-			Username:          user.Username,
-			FullName:          user.FullName,
-			Email:             user.Email,
-			CountryCode:       user.CountryCode,
-			RoleId:            user.RoleID.Int64,
-			LastLoginAt:       timestamppb.New(user.LastLoginAt),
-			CreatedAt:         timestamppb.New(user.CreatedAt),
-			UpdatedAt:         timestamppb.New(user.UpdatedAt),
+			Id:          user.ID,
+			Username:    user.Username,
+			FullName:    user.FullName,
+			Email:       user.Email,
+			CountryCode: user.CountryCode,
+			RoleId:      user.RoleID.Int64,
+			LastLoginAt: timestamppb.New(user.LastLoginAt),
+			CreatedAt:   timestamppb.New(user.CreatedAt),
+			UpdatedAt:   timestamppb.New(user.UpdatedAt),
 		})
 	}
 	return userList
